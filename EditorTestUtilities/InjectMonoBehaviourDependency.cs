16138865019c8d51d5f557f181c2e6d621b36247
Adam Bebko <abebko@bigvikinggames.com>
2025-05-26 14:45:47 -0400

    feat: Injector finalized

--- EditorTestUtilities/InjectMonoBehaviourDependency.cs
+++ EditorTestUtilities/InjectMonoBehaviourDependency.cs
@@ -1,18 +1,39 @@
+using System;
 using System.Reflection;
 using UnityEngine;
 
 namespace EditorTestUtilities
 {
-    public static class TestDependencyInjector
+    /// <summary>
+    /// Tools to help inject dependencies into monobehaviours for testing purposes.
+    /// </summary>
+    public static class DependencyInjector
     {
+        /// <summary>
+        /// Injects a dependency into a private field. Will raise exceptions if
+        /// the inputted type does not match the field type
+        /// </summary>
+        /// <param name="instance"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="value"></param>
         public static void Inject(this MonoBehaviour instance, string fieldName, object value)
         {
-            var field = instance.GetType().GetField(fieldName, 
+            Type monoBehaviourType = instance.GetType();
+            var field = monoBehaviourType.GetField(fieldName, 
                 BindingFlags.NonPublic | BindingFlags.Instance);
             
             if (field != null)
             {
+                if (field.FieldType != value.GetType())
+                {
+                    throw new ArgumentException($"Injector failed: Field {fieldName} is of type {field.FieldType} but value is of type {value.GetType()}");   
+                }
+                
                 field.SetValue(instance, value);
+            }
+            else
+            {
+                throw new ArgumentException($"Injector failed: Field {fieldName} not found on object of type {monoBehaviourType}");  
             }
         }
     }
